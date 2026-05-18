a68aae7b609dec5f6eb5966d47c7ce160205f81e
Manoj Kumar <deekondamanojkumar123@gmail.com>
2026-05-18 18:09:39 +0530

    fix: use production api url

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({
+const api= axios.create({
   baseURL:
     import.meta.env.VITE_API_URL,
 
   withCredentials: true,
 });
 
-export default API;
+export default api;
